2dec41c64aebd800803f9a8ab4194ba0f8a90fde
Abdelwhab Babikr <abdalwhab.babeker@yahoo.com>
2023-10-01 17:12:52 +0400

    Update __manifest__.py

--- custody_clear_request/__manifest__.py
+++ custody_clear_request/__manifest__.py
@@ -23,7 +23,7 @@
         'reports/custody_report.xml',
     ],
     'images': [
-        'static/description/logo.gif',
+        'static/description/icon.gif',
     ],
     'depends': ['base','account','analytic'],
     'installable': True,
